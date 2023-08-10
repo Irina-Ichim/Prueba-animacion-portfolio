5bfef73ee0d73efbd26b818849a272fb9a34741c
Gilda Irina Ichim <onlyirina7@gmail.com>
2023-08-10 13:40:10 +0200

    usando libreria three.js con la ayuda de ChatGpt

--- script3.js
+++ script3.js
@@ -1,42 +1,33 @@
-// Crear el escenario
 const scene = new THREE.Scene();
-
-// Crear una cámara y agregarla al escenario
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 5;
 scene.add(camera);
 
-// Crear una esfera y agregarla al escenario
+// Crea la geometría y el material de la esfera
 const sphereGeometry = new THREE.SphereGeometry(1, 32, 32);
 const sphereTexture = new THREE.TextureLoader().load('img/globe.jpg');
 const sphereMaterial = new THREE.MeshBasicMaterial({ map: sphereTexture });
+
+// Crea la esfera y agrégala a la escena
 const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
 scene.add(sphere);
 
-// Agregar un listener para detectar el movimiento del mouse
+// Agrega un listener para detectar el movimiento del mouse
 window.addEventListener('mousemove', event => {
-  // Calcular la posición de la esfera en función del movimiento del mouse
-  const x = event.clientX / window.innerWidth * 2 - 1;
-  const y = -event.clientY / window.innerHeight * 2 + 1;
+  const x = (event.clientX / window.innerWidth) * 2 - 1;
+  const y = -(event.clientY / window.innerHeight) * 2 + 1;
   sphere.position.set(x, y, 0);
 });
 
-// crea el renderizador
-const renderer = new THREE.WebGLRenderer({
-    canvas: document.querySelector('#bg'),
-});
-
-// establece el tamaño
-renderer.setPixelRatio(window.devicePixelRatio);
+// Crea el renderizador y establece el tamaño
+const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-// cambia el color de fondo del canvas
-renderer.setClearColor(0xffffff, 1);
-
-
-// Renderizar el escenario
+// Renderiza la escena
 function animate() {
   requestAnimationFrame(animate);
   renderer.render(scene, camera);
 }
 animate();
+
